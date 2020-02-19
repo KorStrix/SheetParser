c11ee6256c7012826203a9b04de74ed9c5570eb4
Strix <KorStrix@users.noreply.github.com>
2020-02-19 18:25:39 +0900

    no message

--- UnityProject_Example/Assets/UnitySO_Generator/Editor/UnitySO_GeneratorConfig.cs
+++ UnityProject_Example/Assets/UnitySO_Generator/Editor/UnitySO_GeneratorConfig.cs
@@ -128,7 +128,7 @@ public class UnitySO_GeneratorConfig : ScriptableObject
 
         if(pAsset == null)
         {
-            pAsset = ScriptableObject.CreateInstance(pType);
+            pAsset = CreateInstance(pType);
 
             string strAssetPathAndName = AssetDatabase.GenerateUniqueAssetPath(strFullPath);
             AssetDatabase.CreateAsset(pAsset, strAssetPathAndName);
