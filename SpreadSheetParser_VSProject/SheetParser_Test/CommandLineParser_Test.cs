d3d73fdbf9fc69f949cdf25f69e2b704a5f2218c
Strix <KorStrix@users.noreply.github.com>
2020-03-19 19:01:55 +0900

    no message

---
+++ SpreadSheetParser_VSProject/SheetParser_Test/CommandLineParser_Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpreadSheetParser;
+
+namespace SheetParser_Test
+{
+    [TestClass]
+    public class CommandLineParser_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SpreadSheetParser_VSProject/SheetParser_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SheetParser_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SheetParser_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9ceeb17-7357-450a-884b-1c97b024449a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
