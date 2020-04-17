c76d2ee7b66ab5a48161e9f7edb6aa147a34b58c
KorStrix <Korstrix@gmail.com>
2020-04-17 16:46:06 +0900

    no message

---
+++ SpreadSheetParser_VSProject/SheetParser_Test/TypeData_Tester.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SpreadSheetParser;
+
+namespace SheetParser_Test
+{
+    [TestFixture]
+    public class TypeData_Tester
+    {
+        [Test]
+        public void 타입데이터()
+        {
+        }
+    }
+}
